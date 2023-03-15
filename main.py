83cb6d976a9c9ef023d40215ac46f9d8987504fc
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-15 14:00:34 +0000

    ErRoR cOdE

--- main.py
+++ main.py
@@ -6,14 +6,5 @@ logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
 
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('This will get logged')
-
-
-import logging
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logging.warning('This will get logged to a file')
 
